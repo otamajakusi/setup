00e81913b7983edf23a59e7ee34bb91159645e41
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2022-10-04 09:05:51 +0900

    add git config, pbcopy alias

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -5,6 +5,17 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 
 sudo apt update
 #sudo apt install 
+sudo apt-get install \
+	neovim \
+	git \
+	xsel
+
+git config --global user.email "hiroyuki.obinata@gmail.com"
+git config --global user.name "hiroyuki.obinata"
+git config --global core.editor nvim
+
+echo "alias pbcopy='xsel --clipboard --input'" >> ~/.bash_aliases
+
 
 # adding docker group. reboot is required
 sudo groupadd docker
